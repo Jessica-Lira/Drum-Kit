6e7422d9c8bb37c4ed77863d7d6ab444c809c7f5
Jessica-Lira <70295334+Jessica-Lira@users.noreply.github.com>
2020-09-13 15:51:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+
+//add sounds to button
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+//event key: play the sound according to w, a, s, d, ...
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+  console.log(event.key);
+});
+
+
+//sound-buttons
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+        
+    case "a":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "s":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "d":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+            
+    case "j":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+
+    case "k":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "l":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    default: console.log(key);
+    break
+  }
+}
+
+
+//button:active
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+  
+  //change the style when is clicked
+  activeButton.classList.add("pressed");
+
+  //change again, to normal-mode, before changed
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100); //100 is the time to remove, 1s
+
+}
